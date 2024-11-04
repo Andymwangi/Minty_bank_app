9d8999e4098a623b427ab38b8afe1b1174b17387
Anderson Mwangi <andymwangi3@gmail.com>
2024-11-04 12:29:44 +0300

    implement auth form schema

--- lib/utils.ts
+++ lib/utils.ts
@@ -200,10 +200,10 @@ export const authFormSchema = (type: string) => z.object({
   lastName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
   address1: type === 'sign-in' ? z.string().optional() : z.string().max(50),
   city: type === 'sign-in' ? z.string().optional() : z.string().max(50),
-  state: type === 'sign-in' ? z.string().optional() : z.string().min(2).max(50),
+  state: type === 'sign-in' ? z.string().optional() : z.string().max(50),
   postalCode: type === 'sign-in' ? z.string().optional() : z.string().min(3).max(6),
   dateOfBirth: type === 'sign-in' ? z.string().optional() : z.string().min(3),
-  ssn: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  ssn: type === 'sign-in' ? z.string().optional() : z.string().max(50),
   // both
   email: z.string().email(),
   password: z.string().min(8),
