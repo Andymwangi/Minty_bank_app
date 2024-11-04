6a69c0dc04498fb9b93f4e8d7d1844a2169860b5
Anderson Mwangi <andymwangi3@gmail.com>
2024-11-04 12:02:30 +0300

    Implement authorization bug

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -131,8 +131,8 @@ const AuthForm = ({ type }: { type: string }) => {
                   <CustomInput control={form.control} name='address1' label="Address" placeholder='Enter your specific address' />
                   <CustomInput control={form.control} name='city' label="City" placeholder='Enter your city' />
                   <div className="flex gap-4">
-                    <CustomInput control={form.control} name='state' label="Sub-county" placeholder='Example: Njiru' />
-                    <CustomInput control={form.control} name='postalCode' label="Postal Code" placeholder='Example: 11101' />
+                    <CustomInput control={form.control} name='state' label="Sub-county" placeholder='Example: NJ' />
+                    <CustomInput control={form.control} name='postalCode' label="Postal Code" placeholder='Ex: 11101:5-digit numerals' />
                   </div>
                   <div className="flex gap-4">
                     <CustomInput control={form.control} name='dateOfBirth' label="Date of Birth" placeholder='YYYY-MM-DD' />
